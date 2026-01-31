086209530717d1f4848552eaa50efe9a3ca21a1d
Pan Zhang <pazhang@nvidia.com>
2026-01-31 10:41:50 +0800

    Initial Commit

---
+++ RoadLogger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoadLogger
+//
+//  Created by Pan Zhang on 2026/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoadLogger/RoadLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadLoggerApp.swift
+//  RoadLogger
+//
+//  Created by Pan Zhang on 2026/1/31.
+//
+
+import SwiftUI
+
+@main
+struct RoadLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
